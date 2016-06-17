2b2e126f88df3180bcae7fd86e8309123cace8e3
Daniel Crussi and Nithya Rao <pair+daniel+nithya@allstate.com>
2016-06-17 13:37:40 -0500

    Implemeted portfolio

---
+++ test/portfolio.spec.js
@@ -0,0 +1,69 @@
+const expect = require('chai').expect;
+const Portfolio = require('../lib/portfolio');
+const Stock = require('../lib/stock');
+const nock = require('nock');
+
+describe('Portfolio', () => {
+  beforeEach(() => {
+    nock('http://dev.markitondemand.com')
+    .get('/MODApis/Api/v2/Quote/json?symbol=AAPL')
+    .reply(200, {
+      Name: 'Apple',
+      LastPrice: 100,
+    });
+  });
+  describe('constructor', () => {
+    it('should construct a new Portfolio object', () => {
+      const p1 = new Portfolio('Tech');
+      expect(p1.name).to.equal('Tech');
+    });
+  });
+
+  describe('#addStock', () => {
+    it('should add a stock', () => {
+      const p1 = new Portfolio('Tech');
+      expect(p1.name).to.equal('Tech');
+      const s1 = new Stock('AAPL');
+      s1.shares = 50;
+      s1.purchasePricePerShare = 100;
+      expect(s1.purchasePricePerShare).to.equal(100);
+      expect(s1.shares).to.equal(50);
+      p1.addStock(s1);
+      expect(p1.stocks.length).to.equal(1);
+
+      const s2 = new Stock('GOOG');
+      s2.shares = 10;
+      s2.purchasePricePerShare = 200;
+      expect(s2.purchasePricePerShare).to.equal(200);
+      expect(s2.shares).to.equal(10);
+      p1.addStock(s2);
+      expect(p1.stocks.length).to.equal(2);
+    });
+  });
+
+  describe('#position', () => {
+    it('should return the portfolio position', () => {
+      const p1 = new Portfolio('Tech');
+      const s1 = new Stock('AAPL');
+      s1.shares = 50;
+      s1.purchasePricePerShare = 100;
+      p1.addStock(s1);
+
+      const s2 = new Stock('GOOG');
+      s2.shares = 10;
+      s2.purchasePricePerShare = 200;
+      p1.addStock(s2);
+      expect(p1.stocks.length).to.equal(2);
+      expect(p1.position()).to.equal(7000);
+
+      // s1.shares = 40;
+      // s1.sell(10, (err, cashValue) => {
+      //   expect(err).to.be.null;
+      //   expect(cashValue).to.be.equal(4000);
+      //   expect(s1.shares).to.be.equal(40);
+      //   done();
+      // });
+      // expect(p1.position()).to.equal(6000);
+    });
+  });
+});
--- test/stock.spec.js
+++ test/stock.spec.js
@@ -2,8 +2,26 @@
 
 const expect = require('chai').expect;
 const Stock = require('../lib/stock');
+const nock = require('nock');
+const sinon = require('sinon');
+let clock;
 
 describe('Stock', () => {
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+    nock('http://dev.markitondemand.com')
+    .get('/MODApis/Api/v2/Quote/json?symbol=AAPL')
+    .reply(200, {
+      Name: 'Apple',
+      LastPrice: 100,
+    });
+  });
+
+  after(() => {
+    clock.restore();
+    nock.restore();
+  });
+
   describe('constructor', () => {
     it('should construct a new Stock object', () => {
       const s1 = new Stock('AAPL');
@@ -13,12 +31,14 @@ describe('Stock', () => {
   describe('#purchase', () => {
     it('should purchase a stock', (done) => {
       const s1 = new Stock('aapl');
+      clock.tick(150);
       s1.purchase(50, (err, totalPaid) => {
         expect(err).to.be.null;
-        expect(totalPaid).to.be.above(0);
+        expect(totalPaid).to.equal(5000);
         expect(s1.shares).to.equal(50);
-        expect(s1.name).to.have.length.above(0);
-        expect(s1.purchasePricePerShare).to.be.above(0);
+        expect(s1.purchaseDate.getTime()).to.equal(150);
+        expect(s1.name).to.have.equal('Apple');
+        expect(s1.purchasePricePerShare).to.equal(100);
         done();
       });
     });
@@ -27,12 +47,12 @@ describe('Stock', () => {
     it('should allow to sell shares', (done) => {
       const s1 = new Stock('aapl');
       s1.shares = 100;
-      s1.purchasePricePerShare = 75;
+      // s1.purchasePricePerShare = 75;
       s1.symbol = 'AAPL';
-      s1.name = 'AAPL';
+      // s1.name = 'AAPL';
       s1.sell(50, (err, cashValue) => {
         expect(err).to.be.null;
-        expect(cashValue).to.be.above(0);
+        expect(cashValue).to.be.equal(5000);
         expect(s1.shares).to.be.equal(50);
         done();
       });
@@ -40,12 +60,11 @@ describe('Stock', () => {
     it('should not allow to sell shares if not enough shares are available', (done) => {
       const s1 = new Stock('aapl');
       s1.shares = 50;
-      s1.purchasePricePerShare = 75;
+      // s1.purchasePricePerShare = 75;
       s1.symbol = 'AAPL';
-      s1.name = 'AAPL';
-      s1.sell(100, (err, cashValue) => {
-        expect(err).to.be.not.null;
-        expect(cashValue).to.be.undefined;
+      // s1.name = 'AAPL';
+      s1.sell(100, (err) => {
+        expect(err.message).to.be.equal('Not enough shares to Sell');
         expect(s1.shares).to.be.equal(50);
         done();
       });
